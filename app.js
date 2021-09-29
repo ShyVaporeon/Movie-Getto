e0edfdc1be61d85b6479345e1479cf2a15cf1252
ShyVaporeon <shyvaporeon@gmail.com>
2021-09-29 21:58:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+
+function main() {
+
+    getMovieList();
+
+    findDownloadLink();
+
+    downloadMovie();
+}
+
+function getMovieList() {
+
+    // get top 100 movies 
+    
+    // filter the list
+
+    // convert filtered list into map, movie name mapped to array containing info about movie
+
+    // return result as map
+
+
+    // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+    //###ALTERNATIVE###
+
+    // get top 100 movies 
+
+    // filter list
+
+    // turn movies into objects
+
+    // return array of objects
+
+
+}
+
+function findDownloadLink() {
+
+
+
+}
+
+function downloadMovie() {
+
+    
+
+}
+
+main();
